6825e42fdf89af61ce02b7b37c60a9633ae88529
Thomas <tklein@uni-osnabrueck.de>
2021-02-27 16:40:52 +0100

    indexing and cleanup, not working yet

--- hw2.py
+++ hw2.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import gym
 import numpy as np
 import tensorflow as tf
@@ -7,19 +8,22 @@ from really import SampleManager
 from gridworlds import GridWorld
 from really.utils import (
     dict_to_dataset,
-)  # convenient function for you to create tensorflow datasets
+) 
 
-"""
-DQN homework
-"""
-
+logging.disable(logging.WARNING)
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 class DQN(tf.keras.Model):
 
     def __init__(self, state_size, n_actions, batch_size):
         """
-        Takes the expected size of the state-vector and the number of actions.
+        Constructs a Deep Q-Network.
+
+        state_size: dimension of the state-vector
+        n_actions: number of actions that can be taken
+        batch_size: size of batches during training
         """
+
         super(DQN, self).__init__()
         self.state_size = state_size
         self.n_actions = n_actions
@@ -28,16 +32,18 @@ class DQN(tf.keras.Model):
         self.layer_list = [
             tf.keras.layers.Dense(self.middle_layer_neurons, activation='relu', input_shape=(batch_size, state_size)),
             tf.keras.layers.Dense(self.middle_layer_neurons, activation="relu"),
-            tf.keras.layers.Dense(n_actions)
+            tf.keras.layers.Dense(n_actions)]
 
-        ]
 
     # @tf.function
     def __call__(self, state):
-        # print("initial stat:", state)
+        """
+        Calculates the Q-values for all actions for a given state.
+
+        state: the state vector
+        """
         for layer in self.layer_list:
             state = layer(state)
-            # print("State:", state)
 
         output = {}
         output["q_values"] = state
@@ -45,9 +51,24 @@ class DQN(tf.keras.Model):
 
 
 def train(dqn, state, action, target, optim, loss_func):
+    """
+    Trains the deep Q-Network.
+
+    dqn: the network
+    state: the state in which an action was taken
+    action: the selected action
+    target: the q-values according to the Watkins-Formula
+    optim: the optimizer to be used (e.g. Adam)
+    loss_func: the loss function to be used (MSE)
+    """
+    
+    # converting actions into usable indices
+    a = np.array([[i, val] for i, val in enumerate(action.numpy())])
+
     with tf.GradientTape() as tape:
         prediction = dqn(state)
-        loss = loss_func(target, prediction["q_values"][action])
+        out = tf.gather_nd(prediction["q_values"], tf.constant(a))
+        loss = loss_func(target, out)
         gradients = tape.gradient(loss, dqn.trainable_variables)
     optim.apply_gradients(zip(gradients, dqn.trainable_variables))
 
@@ -96,8 +117,8 @@ if __name__ == "__main__":
     saving_after = 5
 
     gamma = 0.95
-    learning_rate = 0.0001
-    optimizer = tf.keras.optimizers.Adam(learning_rate)
+    learning_rate = 0.01
+    optimizer = tf.keras.optimizers.Adam()
     loss_function = tf.keras.losses.MSE
 
     # keys for replay buffer -> what you will need for optimization
@@ -112,62 +133,50 @@ if __name__ == "__main__":
     )
 
     # initial testing:
-    print("test before training: ")
-    manager.test(test_steps, do_print=True, render=True)
+    # print("test before training: ")
+    # manager.test(test_steps, do_print=True, render=True)
 
     # get initial agent
     agent = manager.get_agent()
 
     for e in range(epochs):
 
-        # training core
-
-        # experience replay
-        print("collecting experience..")
         data = manager.get_data()
         manager.store_in_buffer(data)
 
         # sample data to optimize on from buffer
         sample_dict = manager.sample(sample_size)
-        print(f"collected data for: {sample_dict.keys()}")
+
         # create and batch tf datasets
         data_dict = dict_to_dataset(sample_dict, batch_size=optim_batch_size)
 
-        print("optimizing...")
-
-        # TODO: iterate through your datasets
         loss = 0
         for s, a, r, sn, nd in data_dict:
-            # print("s", s, "a", a, "r", r, "sn", sn, "nd", nd)
-            print('max_q: ', agent.max_q(sn))
-            print('r: ', r)
             q_target = r + gamma * agent.max_q(sn)
-
             loss += train(agent.model, s, a, q_target, optimizer, loss_function)
 
-        new_weights = agent.model.get_weights()
 
         # set new weights
+        new_weights = agent.model.get_weights()
         manager.set_agent(new_weights)
+
         # get new weights
         agent = manager.get_agent()
+
         # update aggregator
         time_steps = manager.test(test_steps)
         manager.update_aggregator(loss=loss, time_steps=time_steps)
-        # print progress
-        print(
-            f"epoch ::: {e}  loss ::: {loss}   avg env steps ::: {np.mean(time_steps)}"
-        )
 
-        # yeu can also alter your managers parameters
+        print(f"epoch ::: {e}  loss ::: {np.round(loss,4)}   avg env steps ::: {np.mean(time_steps)}")
+
+        # you can also alter your managers parameters
         # manager.set_epsilon(epsilon=0.99)
 
         # if e % saving_after == 0:
-        #     # you can save models
         #     manager.save_model(saving_path, e)
 
-    # and load mmodels
-    manager.load_model(saving_path)
+
+    #manager.load_model(saving_path)
     print("done")
     print("testing optimized agent")
     manager.test(test_steps, test_episodes=10, render=True)
