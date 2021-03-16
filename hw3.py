c20a095d88696ce0d2e61726da1475ab5a05257b
Nicolai <nihermann@uos.de>
2021-03-16 01:49:20 +0100

    rewrote sampling without ray and renamed and cleaned up some vars

--- hw3.py
+++ hw3.py
@@ -7,6 +7,7 @@ import tensorflow_probability as tfp
 import ray
 from really import SampleManager
 from gridworlds import GridWorld
+from time import time_ns
 from really.utils import (
     dict_to_dataset, dict_to_dict_of_datasets
 )
@@ -18,58 +19,53 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 class Pi(tf.keras.Model):
 
     def __init__(self, state_size, batch_size):
-
         super(Pi, self).__init__()
         self.state_size = state_size
         self.middle_layer_neurons = 28
         self.second_layer_neurons = 16
 
         self.layer_list = [
-            tf.keras.layers.Dense(self.middle_layer_neurons, activation=tf.nn.leaky_relu, input_shape=(batch_size, state_size), kernel_regularizer='l2'),
-            tf.keras.layers.Dense(self.second_layer_neurons, activation=tf.nn.leaky_relu, kernel_regularizer='l2'),
-            tf.keras.layers.Dense(2, use_bias=False, kernel_regularizer='l2', activation='tanh')]
+            tf.keras.layers.Dense(self.middle_layer_neurons, activation="tanh", input_shape=(batch_size, state_size)),
+            tf.keras.layers.Dense(self.second_layer_neurons, activation="tanh"),
+            tf.keras.layers.Dense(2, use_bias=False, activation='tanh')]
 
-
+    @tf.function
     def call(self, state):
-
         for layer in self.layer_list:
             state = layer(state)
-        
         return state
 
 
 class ValueEstimator(tf.keras.Sequential):
 
     def __init__(self, state_size, batch_size):
-
         super(ValueEstimator, self).__init__()
         self.state_size = state_size
         self.second_layer_neurons = 16
 
-        self.add(tf.keras.layers.Dense(self.second_layer_neurons, activation='relu', kernel_regularizer='l2', input_shape=(batch_size, state_size)))
-        self.add(tf.keras.layers.Dense(1, kernel_regularizer='l2'))
-
+        self.add(
+            tf.keras.layers.Dense(self.second_layer_neurons, activation="relu", input_shape=(batch_size, state_size)))
+        self.add(tf.keras.layers.Dense(32, activation="relu"))
+        self.add(tf.keras.layers.Dense(1, use_bias=False))
 
     # def call(self, state):
 
     #     for layer in self.layer_list:
     #         state = layer(state)
-        
+
     #     return state
 
 
 class ModelWrapper(tf.keras.Model):
-        
+
     def __init__(self, state_size, batch_size, sigma1=0.1, sigma2=0.1):
         super(ModelWrapper, self).__init__()
         self.value_network = ValueEstimator(state_size, batch_size)
         self.pi_network = Pi(state_size, batch_size)
-        self.sigma = tf.constant(np.array([sigma1, sigma2]))
+        self.sigma = tf.constant([sigma1, sigma2], dtype=tf.float32)
 
-
-    # @tf.function
+    @tf.function
     def call(self, x):
-
         # obtain mu of value network
         mu = self.pi_network(x)
 
@@ -84,40 +80,32 @@ class ModelWrapper(tf.keras.Model):
         return output
 
 
-
-def train_pi(action, state, r_sum, value, opt):
-    
+def train_pi(state, action, r_sum, value, opt):
     with tf.GradientTape() as tape:
-        
-        state = np.array(state)
-        state = np.reshape(state, (1,8))
+        mu = agent.model.pi_network(state)
 
-        mue = tf.cast(agent.model.pi_network(state), tf.float64)
-        
         tfd = tfp.distributions
-        dist = tfd.Normal(loc=mue, scale=tf.cast(agent.model.sigma, tf.float64))
-        prob = dist.cdf(action)
+        dist = tfd.Normal(loc=mu, scale=agent.model.sigma)
+        prob_of_action = dist.cdf(action)
 
-        target = tf.math.log(prob)
+        log_probability = tf.math.log(prob_of_action)
 
-        factor = r_sum - tf.cast(tf.squeeze(value), tf.float64)
+        advantage = r_sum - value
 
-        gradients = tape.gradient([factor * target], agent.model.pi_network.trainable_variables)
+        gradients = tape.gradient(advantage * log_probability, agent.model.pi_network.trainable_variables)
 
         opt.apply_gradients(zip(-1 * gradients, agent.model.pi_network.trainable_variables))
 
 
-def train_v(agent, r_sum, state, opt):
-
+def train_v(agent, state, mc_discounted, opt):
     with tf.GradientTape() as tape:
+        value_estimate = agent.model.value_network(state)
 
-        val = agent.model.value_network(tf.expand_dims(state, axis=0))
-
-        loss = tf.keras.losses.MSE(r_sum, val)
+        loss = tf.keras.losses.MSE(mc_discounted, value_estimate)  # + tf.reduce_sum(agent.model.value_network.losses)
         gradients = tape.gradient(loss, agent.model.value_network.trainable_variables)
         opt.apply_gradients(zip(gradients, agent.model.value_network.trainable_variables))
 
-    return loss
+    return float(loss)
 
 
 if __name__ == "__main__":
@@ -139,11 +127,11 @@ if __name__ == "__main__":
     kwargs = {
         "model": ModelWrapper,
         "model_kwargs": model_kwargs,
-        "returns": ['monte_carlo'], 
+        "returns": ['monte_carlo'],
         "environment": 'LunarLanderContinuous-v2',
         "num_parallel": 1,
-        "total_steps": 50, # how many total steps we do
-        "num_steps": 50,
+        "total_steps": 150,  # how many total steps we do
+        "num_steps": 150,
         "action_sampling_type": "continous_normal_diagonal",
         "epsilon": 0.9
     }
@@ -152,22 +140,20 @@ if __name__ == "__main__":
     ray.init(log_to_driver=False)
     manager = SampleManager(**kwargs)
 
-
     #######################
     ## <Hyperparameters> ##
     #######################
     saving_path = os.getcwd() + "/progress_test"
 
-    buffer_size = 30000
+    # buffer_size = 30000
     test_steps = 250
-    epochs = 10
-    sample_size = 1000
-    optim_batch_size = 1
+    epochs = 100
     saving_after = 10
 
-    
+    max_steps = 300
+
     learning_rate = 0.001
-    optimizer = tf.keras.optimizers.Adam() #SGD(learning_rate, momentum=0.8)
+    optimizer = tf.keras.optimizers.Adam(learning_rate)  # SGD(learning_rate, momentum=0.8)
     loss_function = tf.keras.losses.MSE
 
     ########################
@@ -175,79 +161,117 @@ if __name__ == "__main__":
     ########################
 
     # keys for replay buffer -> what you will need for optimization
-    optim_keys = ["state", "action", "reward", "state_new", "not_done"]
+    # optim_keys = ["state", "action", "reward", "state_new", "not_done"]
 
     # initialize buffer
-    manager.initilize_buffer(buffer_size, optim_keys)
+    # manager.initilize_buffer(buffer_size, optim_keys)
 
     # initilize progress aggregator
-    manager.initialize_aggregator(
-        path=saving_path, saving_after=saving_after, aggregator_keys=["loss", "time_steps", "reward"]
-    )
+    # manager.initialize_aggregator(
+    #     path=saving_path, saving_after=saving_after, aggregator_keys=["loss", "time_steps", "reward"]
+    # )
 
     # initial testing:
-    print("Establishing baseline.")
-   # manager.test(test_steps, test_episodes=3, do_print=True, render=True)
+    # print("Establishing baseline.")
+    # manager.test(test_steps, test_episodes=3, do_print=True, render=True)
 
     print("Training the agent.")
 
     # get initial agent
     agent = manager.get_agent()
-    #agent.model.build(optim_batch_size)
+    agent.model.build((1, 8))
+    env = manager.env_instance
 
-    agent.model.build((1,8))
     for e in range(epochs):
-
+        print(">", end="")
         # data = manager.get_data()
         # manager.store_in_buffer(data)
+        # sample_dict = manager.sample(1, from_buffer=False)
 
-        sample_dict = manager.sample(1, from_buffer=False) # 
+        states, actions, cumulative_mc_rewards = [], [], [],
+
+        # get s0 and reshape it to make it batch size 1
+        s = env.reset()
+        for i in range(max_steps):
+            s = np.array(s).reshape((1, 8))
+
+            # obtain the mus for both action and sample the action from a gaussian afterwards.
+            mu = agent.model.pi_network(s)
+            a = tf.random.normal(shape=(2,), mean=mu, stddev=agent.model.sigma)
+
+            # take the step in the env
+            next_s, reward, done, _ = env.step(a.numpy().reshape(2))
+            # next_s = np.array(next_s).reshape((1, 8))  # reshape immediately for the next iteration and for convenience.
+
+            # save everything
+            states.append(s)
+            actions.append(a)
+            # if list is empty we only need the reward as first value.
+            # else, we are discounting cumulatively to obtain mc_rewards.
+            cumulative_mc_rewards.append(reward if cumulative_mc_rewards == [] else cumulative_mc_rewards[-1] + (gamma ** i * reward))
+            # new_states.append(next_s)
+
+            s = next_s  # the posterior of today is tomorrows prior.
+
+            if done:
+                # if we reach a terminal state our estimate for the remaining trajectory is 0.
+                cumulative_mc_rewards.append(0)
+                break
+
+        if not done:
+            next_s = np.array(next_s).reshape((1, 8))  # bring it in shape :D
+            # if we did not managed to reach a terminal state we will estimate the remaining trajectory.
+            R = agent.model(next_s)['value_estimate']
+            cumulative_mc_rewards.append(float(R))
+
+        cumulative_mc_rewards = tf.constant(cumulative_mc_rewards, tf.float32)
+        env.close()
 
         # create and batch tf datasets
-        data_dict = dict_to_dict_of_datasets(sample_dict, batch_size=optim_batch_size)
+        # data_dict = dict_to_dict_of_datasets(sample_dict, batch_size=optim_batch_size)
 
-        def get_end(data):
-            for i, d in enumerate(data):
-                if d == 0:
-                    return i, True
-            return len(data)-1, False
-        
-        end, terminal = get_end(sample_dict['not_done'])
+        # def get_end(data):
+        #     for i, d in enumerate(data):
+        #         if d == 0:
+        #             return i+1, True
+        #     return len(data)-1, False
 
+        # end, terminal = get_end(sample_dict['not_done'])
 
-        states = sample_dict['state'][:end]
-        actions = sample_dict['action'][:end]
-        new_states = sample_dict['state_new'][:end]
-        rewards = sample_dict['reward'][:end]
-        mc_rewards = [ sum([gamma**j*r for j,r in enumerate(rewards[i:])]) for i in range(len(rewards)) ]
+        # states = sample_dict['state'][:end]
+        # actions = sample_dict['action'][:end]
+        # new_states = sample_dict['state_new'][:end]
+        # rewards = sample_dict['reward'][:end]
+        # mc_rewards = [sum([gamma**j*r for j, r in enumerate(rewards[i:])]) for i in range(len(rewards))]
         # mc_rewards = sample_dict['monte_carlo'][:end]
 
         # print("Normal rewards: ", rewards)
         # print("MC rewards: ", mc_rewards)
-        
-        if terminal: # TODO think about this
-            R = agent.model(states[end])['value_estimate']
-            mc_rewards.apend(gamma ** end * R)
+
+        # if terminal: # TODO think about this
+        #     end_state = states[end-1].reshape((1, 8))
+        #     R = agent.model(end_state)['value_estimate']
+        #     mc_rewards.append(gamma ** end * R)
 
         loss = 0
-
+        
+        # fix our value network to prevent inaccurate updates.
         stormtrooper = tf.keras.models.clone_model(agent.model.value_network)
 
         it = 1
-        for s, a, sn, mc_r in zip(states, actions, new_states, mc_rewards): 
+        for s, a, mc_r in zip(states, actions, cumulative_mc_rewards):
+            value_estimate = stormtrooper(s)
 
-            value = stormtrooper(tf.expand_dims(s, axis=0))
-
-            train_pi(a, s, mc_r, value, optimizer)
-            loss_v = train_v(agent, mc_r, s, optimizer)
+            train_pi(s, a, mc_r, value_estimate, optimizer)
+            loss_v = train_v(agent, s, mc_r, optimizer)
 
             # old average * (n-1)/n + new value /n
-            loss = loss * (it-1)/it + loss_v / it
+            loss = loss * (it - 1) / it + loss_v / it
             it += 1
 
         # update with new weights
         new_weights = agent.model.get_weights()
-        
+
         agent.set_weights(new_weights)
         manager.set_agent(new_weights)
 
@@ -255,13 +279,22 @@ if __name__ == "__main__":
         agent = manager.get_agent()
 
         # update aggregator
-        time_steps, rewards = manager.test(test_steps, render=False, evaluation_measure="time_and_reward")
-       
-        manager.update_aggregator(loss=loss, time_steps=time_steps, reward=rewards)
+        if e % 10 == 0:  # every 10 epochs do:
+            if e % 20 == 0:  # render test.
+                time_steps, rewards = manager.test(
+                    test_steps,
+                    test_episodes=10,
+                    render=True,
+                    evaluation_measure="time_and_reward"
+                )
+            else:  # or do not render the test and save model.
+                time_steps, rewards = manager.test(test_steps, render=False, evaluation_measure="time_and_reward")
+                # manager.save_model("./models", e, "LunarLander")
 
-        print(f"epoch ::: {e}  loss ::: {loss}   reward ::: {np.mean(rewards)}   avg env steps ::: {np.mean(time_steps)}")
+            # manager.update_aggregator(loss=loss, time_steps=time_steps, reward=rewards)
+            print(f"\nepoch ::: {e}  loss ::: {loss}   reward ::: {np.sum(rewards)}   avg env steps ::: {np.mean(time_steps)}")
 
-    print("Done.")
+    print("\nDone.")
     print("Testing optimized agent.")
 
     manager.test(100, test_episodes=10, render=True, do_print=True)
