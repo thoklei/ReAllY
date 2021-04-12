b1031b1bf74c5ec93a0f78092f5800ec3229bf90
Nicolai <nihermann@uos.de>
2021-04-12 19:55:07 +0200

    model conversion

--- ppo_model.py
+++ ppo_model.py
@@ -3,68 +3,43 @@ from tensorflow.keras import Model
 from tensorflow.keras.layers import (Dense, Input, Lambda)
 
 
+def dense_block(X, units_list, activation, name=None):
+    """
+    Adds a block of Dense layers for a functional model architecture.
+    The number of layers is specified by the length of arguments in the units_list.
+    """
+    for i, units in enumerate(units_list):
+        if name is None:
+            X = Dense(units, activation=activation)(X)
+        else:
+            X = Dense(units, activation=activation, name=name.format(i))(X)
+    return X
+
+
 class A2C(Model):
     def __init__(self, layers, action_dim, state_dim):
-        super(A2C, self).__init__()
-        self.mu_layer = [
-            tf.keras.layers.Dense(
-                units=num_units,
-                activation='relu',
-                name=f'Policy_mu_{i}'
-                ) for i, num_units in enumerate(layers)]
+        inp = Input((state_dim,))
+        mu = dense_block(inp, layers, 'relu', "Policy_mu_{}")
+        mu_out = Dense(action_dim, activation=None, name="Policy_mu_readout")(mu)
 
-        self.readout_mu = tf.keras.layers.Dense(units=action_dim,
-                                                activation=None,
-                                                name='Policy_mu_readout'
-                                                )
+        sigma = dense_block(inp, layers, 'relu', "Policy_sigma_{}")
+        sigma_out = Dense(units=action_dim, activation=None, name='Policy_sigma_readout')(sigma)
 
-        self.sigma_layer = [
-            tf.keras.layers.Dense(
-                units=num_units,
-                activation='relu',
-                name=f'Policy_sigma_{i}'
-                ) for i, num_units in enumerate(layers)]
+        value = dense_block(inp, layers, 'relu', "Value_layer_{}")
+        value_out = Dense(units=1, activation=None, name='Value_readout')(value)
 
-        self.readout_sigma = tf.keras.layers.Dense(units=action_dim,
-                                                   activation=None,
-                                                   name='Policy_sigma_readout'
-                                                   )
+        out = Lambda(
+            lambda outputs: {
+                "mu": tf.squeeze(outputs[0]),
+                "sigma": tf.squeeze(tf.abs(outputs[1])),
+                "value_estimate": tf.squeeze(outputs[2])
+            }
+        )([mu_out, sigma_out, value_out])
 
-        self.value_layer = [
-            tf.keras.layers.Dense(
-                units=num_units,
-                activation='relu',
-                name=f'Value_layer_{i}'
-                ) for i, num_units in enumerate(layers)]
-
-        self.readout_value = tf.keras.layers.Dense(units=1,
-                                                   activation=None,
-                                                   name='Value_readout'
-                                                   )
-
-    @tf.function
-    def call(self, input_state):
-        output = {}
-        mu_pred = input_state
-        sigma_pred = input_state
-        value_pred = input_state
-        for layer in self.mu_layer:
-            mu_pred = layer(mu_pred)
-        for layer in self.sigma_layer:
-            sigma_pred = layer(sigma_pred)
-        for layer in self.value_layer:
-            value_pred = layer(value_pred)
-
-        # Actor
-        output["mu"] = tf.squeeze(self.readout_mu(mu_pred))
-        output["sigma"] = tf.squeeze(tf.abs(self.readout_sigma(sigma_pred)))
-        # Critic
-        output["value_estimate"] = tf.squeeze(self.readout_value(value_pred))
-        return output
+        super(A2C, self).__init__(inp, out)
 
 
 class TargetNetwork(Model):
-
     def __init__(self, layers, k, state_dim):
         """
         Constructor for the RND-target network.
@@ -72,75 +47,12 @@ class TargetNetwork(Model):
         layers = list of layer sizes
         k = dimensionality of the embedding space
         """
+        inp = Input((state_dim,))
+        X = dense_block(inp, layers, "relu", "")
+        out = Dense(units=k, activation=None, name="output")(X)
 
-        super(TargetNetwork, self).__init__()
+        super(TargetNetwork, self).__init__(inp, out)
 
-        self.rnd_layer = [
-            tf.keras.layers.Dense(
-                units=num_units,
-                activation='relu') for i, num_units in enumerate(layers)]
-
-        self.readout = tf.keras.layers.Dense(units=k, activation=None)
-
-
-    @tf.function
-    def call(self, input_state):
-
-        embedding = input_state
-
-        for layer in self.rnd_layer:
-            embedding = layer(embedding)
-
-        readout = self.readout(embedding)
-
-        return readout
-
-# def dense_block(X, units_list, activation, name=None):
-#     for i, units in enumerate(units_list):
-#         if name is None:
-#             X = Dense(units, activation=activation)(X)
-#         else:
-#             X = Dense(units, activation=activation, name=name.format(i))(X)
-#     return X
-#
-#
-# class A2C(Model):
-#     def __init__(self, layers, action_dim, state_dim):
-#         inp = Input((state_dim,))
-#         mu = dense_block(inp, layers, 'relu', "Policy_mu_{}")
-#         mu_out = Dense(action_dim, activation=None, name="Policy_mu_readout")(mu)
-#
-#         sigma = dense_block(inp, layers, 'relu', "Policy_sigma_{}")
-#         sigma_out = Dense(units=action_dim, activation=None, name='Policy_sigma_readout')(sigma)
-#
-#         value = dense_block(inp, layers, 'relu', "Value_layer_{}")
-#         value_out = Dense(units=1, activation=None, name='Value_readout')(value)
-#
-#         out = Lambda(
-#             lambda outputs: {
-#                 "mu": tf.squeeze(outputs[0]),
-#                 "sigma": tf.squeeze(tf.abs(outputs[1])),
-#                 "value_estimate": tf.squeeze(outputs[2])
-#             }
-#         )([mu_out, sigma_out, value_out])
-#
-#         super(A2C, self).__init__(inp, out)
-#
-#
-# class TargetNetwork(Model):
-#
-#     def __init__(self, layers, k, state_dim):
-#         """
-#         Constructor for the RND-target network.
-#
-#         layers = list of layer sizes
-#         k = dimensionality of the embedding space
-#         """
-#         inp = Input((state_dim,))
-#         X = dense_block(inp, layers, "relu", "")
-#         out = Dense(units=k, activation=None, name="output")(X)
-#
-#         super(TargetNetwork, self).__init__(inp, out)
 
 @tf.function
 def train_on_batch_ppo(
